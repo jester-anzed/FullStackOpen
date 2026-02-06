8b675fa3c4773064af0cf23cd3557a50ab8fe266
Marc <jester.ismael@gmail.com>
2026-02-05 22:10:17 -0500

    Prac

--- part2/Prac/src/App.jsx
+++ part2/Prac/src/App.jsx
@@ -3,37 +3,57 @@ import { useState } from 'react'
 
 
 const Movies = ({movies, filter}) => {
-  const filteredMovies = movies.filter(movie => movie.title === filter)
+  const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(filter.toLowerCase()))
+  return (
+    filteredMovies.map(movie => <Movie key={movie.id} movies={movie} />)
+  )
+}
+
+const Movie = ({movies}) => <div>Movie: {movies.title} Year: {movies.year} </div>
+  
+const Filter = ({filter, setFilter}) => <div><input value={filter} onChange={setFilter}/></div>
+  
+
+const MovieForm = (movies, setter) => {
+
+  const [movieName, setMovie] = useState('')
+  const [movieYear, setYear] = useState('')
+
+  const movieNameChange = (event) => {
+    setMovie(event.target.value)
+  }
+
+  const movieYearChange = (event) => {
+    setYear(event.target.value)
+  }
+
+  const addMovie = (event) => {
+    event.preventDefault()
+    
+    const nameObject = {  
+      title: movieName,
+      year: movieYear,
+      id: String(movies.length + 1)
+    }
+    setter(movies.concat(nameObject))
+    setMovie("")
+    setYear("")
+  }
+
 
   return (
-    filteredMovies.map(movie => <Movie movie={movie} />)
+    <form onSubmit={addMovie}>
+      <div>
+        <div>Name: <input value={movieName} onChange={movieNameChange}/></div>
+        <div>Year: <input value={movieYear} onChange={movieYearChange}/></div>
+        <div>Debug: {movieName}</div>
+        <div>Debug: {movieYear}</div>
+        <div><button type="submit">Submit</button></div>
+      </div>
 
+    </form>
   )
 }
-
-const Movie = ({movie}) => {
-  console.log(movie)
-  return (
-    <div key={movie.id}>
-      <div>Title: {movie.title}</div>
-      <div>Year: {movie.year}</div>
-    </div>
-  )
-}
-
-
-
-const Filter = ({filter, setFilter}) => {
-
-  return(
-    <>
-    <div><input value={filter} onChange={setFilter}/></div>
-    <div>Debug: {filter}</div>
-    </>
-  )
-}
-
-
 
 const App = () => {
   const [movies, setMovies] = useState([
@@ -52,7 +72,14 @@ const App = () => {
 
   return (
     <>
-      <Filter value={filteredMovie} onChange={handleFilter} />
+      <h2>Filtered Movies:</h2>
+      <Filter filter={filteredMovie} setFilter={handleFilter} />
+
+      <h2>Movie List</h2>
+      <Movies movies={movies} filter={filteredMovie} />
+      
+      <h2>Add Movie</h2>
+      <MovieForm movies={movies} setter={setMovies}/>
     </>
   )
 
