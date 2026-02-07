1116a406f152854807316057f4125a53551b253b
Marc <jester.ismael@gmail.com>
2026-02-07 11:40:50 -0500

    Exercise 11 Done

---
+++ part2/Exercise 2.11/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Person = ({person}) => <div>{person.name} {person.number}</div>
+
+const Persons = ({data, filter}) => {
+  const showPeople = data.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  return (
+    showPeople.map(person => <Person key={person.id} person={person} />)
+  )
+}
+const Filter =  ({person, filter}) => <div>Filter: <input value={person} onChange={filter} /></div>
+
+
+const Add = ({ persons,  setterPerson}) => {
+
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumChange = (event) => {54
+    setNewNum(event.target.value)
+
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault()
+
+    if (persons.find(person => person.name === newName)) {
+      return alert(`${newName} is already added to phonebook`)
+    }
+
+    else {
+
+      const nameObject = {  
+        name: newName,
+        number: newNum,
+        id: String(persons.length + 1)
+      }
+      setterPerson(persons.concat(nameObject))
+      setNewName("")
+      setNewNum("")
+    }
+  }
+  return (
+    <form onSubmit={submitForm}>
+      <div>name: <input value={newName} onChange={handleNameChange} /></div>
+      <div>number: <input value={newNum} onChange={handleNumChange} /></div>
+      <div> <button type="submit">Add</button> </div>
+    </form>
+    )
+}
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+  ])
+
+  const [filterName, setFilterName] = useState('')
+
+  const handleFilter = (event) => {
+    setFilterName(event.target.value)
+  }
+
+
+  useEffect(() => {
+    console.log("effect")
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter person={filterName} filter={handleFilter} />
+
+      <h3>Add a new</h3>
+
+      <Add persons={persons} setterPerson={setPersons} />
+
+      <h3>Numbers</h3>
+      
+      <Persons data={persons} filter={filterName}/>  
+
+    </div>
+  )
+}
+
+export default App
+
--- part2/Prac/src/App.jsx
+++ part2/Prac/src/App.jsx
@@ -1,20 +1,42 @@
 import { useState } from 'react'
 
 
+const Watched = ({movies}) => {
+  const count = movies.filter(movie => movie.watched).length 
+  return <div>Watched Movies: {count}</div>
 
-const Movies = ({movies, filter}) => {
-  const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(filter.toLowerCase()))
-  return (
-    filteredMovies.map(movie => <Movie key={movie.id} movies={movie} />)
-  )
 }
 
-const Movie = ({movies}) => <div>Movie: {movies.title} Year: {movies.year} </div>
+const Unwatched = ({movies}) => {
+  const count = movies.filter(movie => !movie.watched).length 
+  return <div>Unwatched Movies: {count}</div>
+  
+}
+
+const statusButton = () => {
+
+
+}
+
+
+const Movies = ({movies, filter, onDelete, watched, status}) => {
+  const filtered = movies.filter(movie => movie.title.toLowerCase().includes(filter.toLowerCase()))
+  return filtered.map(movie => <Movie key={movie.id} movies={movie} onDelete={onDelete} watched={watched} />)
+  
+}
+
+const Movie = ({movies, onDelete, watched}) => 
+<div>
+  Movie: {movies.title} 
+  Year: {movies.year} 
+  <button onClick={() => onDelete(movies.id)}>Delete</button> 
+  <button onClick={() => watched(movies.id)}>{movies.watched ? "Watched" : "To Watch"}</button>
+</div>
   
 const Filter = ({filter, setFilter}) => <div><input value={filter} onChange={setFilter}/></div>
   
 
-const MovieForm = (movies, setter) => {
+const MovieForm = ({movies, setter}) => {
 
   const [movieName, setMovie] = useState('')
   const [movieYear, setYear] = useState('')
@@ -29,31 +51,37 @@ const MovieForm = (movies, setter) => {
 
   const addMovie = (event) => {
     event.preventDefault()
-    
-    const nameObject = {  
-      title: movieName,
-      year: movieYear,
-      id: String(movies.length + 1)
-    }
+    if (movies.find(movie => movie.title === movieName)) {
+      setMovie("")
+      setYear("")
+      return alert(`${movieName} is already on the list!`)
+        
+    } else {
+
+      const nameObject = {  
+        title: movieName,
+        year: movieYear,
+        watched: true,
+        id: String(movies.length + 1)
+      }
     setter(movies.concat(nameObject))
     setMovie("")
     setYear("")
+    }
   }
-
 
   return (
     <form onSubmit={addMovie}>
       <div>
         <div>Name: <input value={movieName} onChange={movieNameChange}/></div>
-        <div>Year: <input value={movieYear} onChange={movieYearChange}/></div>
-        <div>Debug: {movieName}</div>
-        <div>Debug: {movieYear}</div>
+        <div>Year: <input type="number" value={movieYear} onChange={movieYearChange}/></div>
         <div><button type="submit">Submit</button></div>
       </div>
-
     </form>
   )
 }
+
+
 
 const App = () => {
   const [movies, setMovies] = useState([
@@ -62,7 +90,8 @@ const App = () => {
   { title: 'Interstellar', year: 2014, watched: false, id: 3 }
   ])
 
-  const [filteredMovie, setFilter] = useState('')
+  const [filter, setFilter] = useState('')
+  const [statusFilter, setStatusFilter] = useState('all')
 
 
   const handleFilter = (event) => {
@@ -70,16 +99,41 @@ const App = () => {
     console.log(event.target.value)
   }
 
+  const handleDelete = (id) => {
+    const newList = movies.filter(movie => movie.id !== id)
+    setMovies(newList)
+  }
+  
+  const toggleWatch = (id) => {
+    const newList = movies.map(movie => movie.id === id ? {...movie, watched: !movie.watched}: movie)
+    setMovies(newList)
+  }
+
+ const sortMovies = () => {
+    const sorted = [...movies].sort((a, b) => a.title.localeCompare(b.title))
+    setMovies(sorted)
+  }
+
+  const sortYear = () => {
+    const sorted = [...movies].sort((a,b) => a.year - b.year)
+    setMovies(sorted)
+  }
+
   return (
     <>
       <h2>Filtered Movies:</h2>
-      <Filter filter={filteredMovie} setFilter={handleFilter} />
+      <Filter filter={filter} setFilter={handleFilter} />
+      
 
       <h2>Movie List</h2>
-      <Movies movies={movies} filter={filteredMovie} />
-      
+      <button onClick={sortMovies}>Sort By Name</button> <button onClick={sortYear}>Sort By Year</button>
+      <Movies movies={movies} filter={filter} onDelete={handleDelete} watched={toggleWatch} status={statusFilter} />
+
+      <h2>Watched / Unwatched</h2>
+      <Watched  movies={movies} /> <Unwatched movies={movies} />
+
       <h2>Add Movie</h2>
-      <MovieForm movies={movies} setter={setMovies}/>
+      <MovieForm movies={movies} setter={setMovies} />
     </>
   )
 
