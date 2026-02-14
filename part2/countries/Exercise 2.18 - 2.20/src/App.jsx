1abb68687f47ab8803021993edd62bb46a6d00b5
Marc <jester.ismael@gmail.com>
2026-02-14 14:11:06 -0500

    Exercise 2.18 Done

--- part2/countries/Exercise 2.18 - 2.20/src/App.jsx
+++ part2/countries/Exercise 2.18 - 2.20/src/App.jsx
@@ -15,30 +15,48 @@ const App = () => {
     event.preventDefault()
   }
 
-
-
    const Filter = () => {
       const filter = countries.filter(country => country.toLowerCase().includes(userInput.toLowerCase()))
 
       useEffect(() => {
-        if (filter.length === 1) {
+        if (filter.length === 1 ) {
           axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${filter[0].toLowerCase()}`)
           .then(response => {
-            console.log(response.data.name
-              
-            )
-        })
-       }
-      }, [filter] )
+            if (!value || value.name.common !== response.data.name.common) {
+              setValue(response.data)
+            }
+          })
+        } else if (value !== null) {
+            setValue(null)
+        }
+      }, [userInput])
+    
 
-      return (
-        <div>"Yikes"</div>
-      )
+      if (filter.length > 10 ) return "Too many to specify" 
+      if (filter.length < 10 && filter.length > 1) return <div>{filter.map(d => <div key={d}>{d}</div>)}</div>
 
-    }
+      if (filter.length === 1 && value) {
+        return ( 
+          <div key={value.name.common}>
+
+            <h1>{value.name.common}</h1>
+            <div>Capital: {value.capital}</div>
+            <div>Area: {value.area}</div>
+
+            <h1>Languages</h1>
+            <ul>{Object.values(value.languages).map(d => <li key={d}>{d}</li>)}</ul>
+            
+            <img src={value.flags.png} alt={value.flags.alt}/>
 
 
-      
+          </div>
+
+        )
+
+      }
+    
+  }
+  
      
 
   useEffect(() => {
