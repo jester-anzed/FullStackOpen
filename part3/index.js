feb05b3cda5c20e0c20ecfd531c55f83da5abb35
Marc <jester.ismael@gmail.com>
2026-02-17 20:45:57 -0500

    Part 3 Progress

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: "1",
@@ -21,6 +23,33 @@ let notes = [
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 
