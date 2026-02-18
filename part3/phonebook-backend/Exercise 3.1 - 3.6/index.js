241fd1dfb54f9cdeb2a922c5ab8dd05c3e0bf072
Marc <jester.ismael@gmail.com>
2026-02-17 21:09:37 -0500

    Exercise 3.2 Done

--- part3/phonebook-backend/Exercise 3.1 - 3.6/index.js
+++ part3/phonebook-backend/Exercise 3.1 - 3.6/index.js
@@ -34,6 +34,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const now = new Date();
+  response.setHeader('Content-Type', 'text/html')
+  response.write(`<p>Phonebook has info for ${persons.length} people</p>`)
+  response.write(`<p>${now}</p>`)
+  response.end()
+
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
