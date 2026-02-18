dc15561288161d1bbc41721c40eeb141606c1a08
Marc <jester.ismael@gmail.com>
2026-02-18 10:27:07 -0500

    Exercise 3.7 Done

--- part3/phonebook-backend/Exercise 3.1 - 3.8/index.js
+++ part3/phonebook-backend/Exercise 3.1 - 3.8/index.js
@@ -3,6 +3,20 @@ const app = express()
 
 app.use(express.json())
 
+const morgan = require('morgan')
+
+const requestMorgan = (tokens, request, response ) => {
+  return [
+  tokens.method(request, response),
+  tokens.url(request, response),
+  tokens.status(request, response),
+  tokens.res(request, response, 'content-length'), '-',
+  tokens['response-time'](request, response), 'ms',
+  ].join(' ')
+}
+
+app.use(morgan(requestMorgan))
+
 let persons = [
     { 
       "id": "1",
