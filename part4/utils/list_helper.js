9a0bf9636c7c3d74c7da7d5f9542507214ebed5b
Marc <jester.ismael@gmail.com>
2026-02-27 10:35:32 -0500

    Part 4.4 Done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,30 @@ const dummy = (blogs) => {
   return 1
 }
 
+
+
+
+const totalLikes = (likes) => {
+
+    if (likes.length === 0) {
+        return likes.length
+    } 
+    
+    if (likes.length === 1) {
+        return likes[0].likes
+    } 
+    
+    if (likes.length > 1) {
+        let blog = likes.map(like => like.likes)
+
+    
+
+        return blog.reduce((sum, item) => sum + item, 0)
+    }
+}
+
+
 module.exports = {
-  dummy
+    dummy,
+    totalLikes
 }
