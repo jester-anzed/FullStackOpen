264297000e1f2917587bc3a8c450a39778c30ad9
Marc <jester.ismael@gmail.com>
2026-02-26 22:00:09 -0500

    Part 4.2 Done

--- part4/app.js
+++ part4/app.js
@@ -1,8 +1,33 @@
 const express = require('express')
-
-
-
 const app = express()
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 
-app.use(express.json))
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRouter)
+
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,41 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 
-const app = express()
-
-const blogSchema =  mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = `mongodb+srv://fullstackopen:test1234@cluster0.q9u15xv.mongodb.net/Blog?appName=Cluster0`
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    console.log(result)
-    response.status(201).json(result)
-  })
-  .catch(error => {
-    response.status(404).json({ error: "incorrect" })
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
